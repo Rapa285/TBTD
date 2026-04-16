4c7d65504ec30d11aac14ac1bda5caf28c69ea9c
Rafa <pradipta.mahesa@bosnetdis.com>
2026-04-16 15:58:18 +0700

    feat : camera prefab

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -3,19 +3,21 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField]
+    private int health;
+    [SerializeField]
+    private int damage;
+    [SerializeField]
+    private int movementSpeed;
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("pip");
+        // Debug.Log("pip");
 
         if (other.gameObject.CompareTag("MapEnd"))
         {
             Debug.Log("Enemy reached the end!");
             Destroy(gameObject);
-        }
-        else
-        {
-            Debug.Log("hit "+other.gameObject.tag);
         }
     }
     // public abstract void damage();
